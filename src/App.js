427533fd633732cdadf83b6ed2523238e0ee614e
Mirim_Lee <leemirim9654@gmail.com>
2018-06-24 23:40:45 +0900

    set propTypes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 import Movie from './Movie';
 
 //자식 컴포넌트에게 던져줄 데이터를 저장하는 배열이다.
-
+const movies = [
+  {
+    title: "A",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvDeaFigARYsk5br4eRZpYsShV_W3o1tP-bJsZtay3p3gHV1oCvg"
+  },
+  {
+    title: "B",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdBMNAiNcY0svwf-PbgaaxRoBoQfAqKU9j_D1xSISa4hw-HExe"
+  },  
+  {
+    title: "C",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzQQyELeg2UEu0BWuiR8253WKVdDqrpHD0BzTfm6PK9jF8mDBOnA"
+  },
+  {
+    title: "D",
+    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD3M1nKRdp5IEj9XJGTBXLZ6aNzCFSIb3JRO95m0XHVFoTYMo-wg"
+  },
+]
 
 
 //중요! -> 모든 컴포넌트는 render 함수를 갖고 있다.
@@ -14,8 +31,11 @@ class App extends Component {
       //React에서 자바스크립트 속 html과 유사한 이 문법을 JSX라고 부른다.//
       //JSX는 리액트 컴포넌트를 만들 때 사용하는 언어이다.//
       <div className="App">
-        {movies.map(movie => {
-          <Movie title={movie.title} poster={movie.poster} />
+        {movies.map((movie, index) => {
+          //index는 현재 cycle 상황에서 movie 안에 들어온 argument의 index이다.
+          //즉, movie의 리스트 속 index
+          //복수의 child 컴포넌트는 각각 고유한 key값을 갖어야한다.
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
     );
--- src/Movie.js
+++ src/Movie.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
+
 
 //컴포넌트는 항상 render을 해야한다. = 컴포넌트는 항상 render()함수를 갖고 있어야 한다.//
 //컴포넌트 생성 > 렌터 > 리턴 > JSX (html과 유사) 
+
 class Movie extends Component{
+    //App(father)이 보내준 props가 Movie(child)가 원하는 데이터 tpye인지 체크하는 부분
+    //보내준 props type과 원하는 props type이 다르다면, console에서 Error 메세지가 뜬다. 
+    static propTypes = {
+        //"title props는 string type으로만 출력한다."의미
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string
+    }
+    //child component에서 father component가 준 props에 접근하는 방법
+    // -> By calling '{this.props.~}'
     render(){
         return(
             <div>
-            <MoviePoster />                
-            <h1>Hello world!</h1>
+            <MoviePoster poster={this.props.poster}/>           
+            <h1>{this.props.title}</h1>
             </div>
         );
     }
 }
 
 class MoviePoster extends Component{
+    //Movie(father)이 보내준 props가 MoviePoster(child)가 원하는 데이터 tpye인지 체크하는 부분
+    //보내준 props type과 원하는 props type이 다르다면, console에서 Error 메세지가 뜬다. 
+    static propTypes = {
+        poster: PropTypes.string
+    }
+
     render(){
         return(
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2yWk-LBi5L5Vs_qFSU7doEIGPI5oUtBCLqbnFPbp1tkHcZdIfiQ"/>
+            //{}속에 해당 porps를 지정해줘야한다.    
+            <img src={this.props.poster} alt="Movie Poster" />
         );
     }
 }
