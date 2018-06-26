81d8289bd5fda938e1f531366bf417889c14a865
Mirim_Lee <leemirim9654@gmail.com>
2018-06-26 22:28:31 +0900

    Updating Component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
 //state를 바꿀때는 setState를 이용해서 변경해줘야한다. (바로 this.~ 이런 식으로 쓰면 안된다.)
 //state가 변경되면, 새로운 state와 함께 다시 render한다.
   state = {
-  }
+  };
 
   componentDidMount(){
     this._getMovies();
@@ -20,20 +20,30 @@ class App extends Component {
   
   _renderMovies = () => {
       //map 함수는 각각의 고유한 props를 갖고 있는 <Movie />들의 배열을 출력한다.
-    const movies = this.state.movies.map((movie) => {
+    const movies = this.state.movies.map(movie => {
+      console.log(movie)
+      console.log(movie.genres)
       //index는 현재 cycle 상황에서 movie 안에 들어온 argument의 index이다.
       //즉, movie의 리스트 속 index
       //복수의 child 컴포넌트는 각각 고유한 key값을 갖어야한다.
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
-    })
-    return movies
-  }
+      return (
+        <Movie 
+          title={movie.title_english} 
+          poster={movie.medium_cover_image} 
+          key={movie.id} 
+          genres={movie.genres}
+          synopsis={movie.synopsis} 
+          />
+        );    
+    });
+    return movies;
+  };
+
 
   _getMovies = async () => {
     //await = callApi()가 끝날 때까지 기다린다(성공여부와 상관없이)
     //callApi()가 return한 값을 movies 배열안에 대입한다.
     const movies = await this._callApi();
-    console.log(movies)
     this.setState({
       movies : movies
     })
@@ -41,7 +51,7 @@ class App extends Component {
   
   _callApi = () => {
     //fetch는 url을 AJAX로 불러올 수 있다는 장점이 있다.
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count?sort_by=rating')
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
     //fetch작업이 끝나면(성공적 수행이 아니더라도), then을 불러온다.
     //fetch 작업이 완료되면, fetch된 값을 reponser에 저장하고, 0101 바이너라 코드로 된
     // response를 json으로 변형해준다.
--- src/Movie.js
+++ src/Movie.js
@@ -3,35 +3,61 @@ import PropTypes from 'prop-types';
 import './Movie.css';
 
 //이미 처음부터 props를 사용하기 때문에 this.~ 가 필요없다.
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
+    console.log(genres)
     return (
-        <div>
-        <MoviePoster poster={poster} />
-        <h1>{title}</h1>
-        </div>
-    )
-}
+        <div className="Movie">
 
-Movie.propTypes = {
-    title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title} />
+            </div>
+
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
+        </div>
+    );
 }
 
 //Dumb Component: state가 없는 컴포넌트
 //return만을 위한 컴포넌트이다.
 //컴포넌트 lifecycle이 없다. 또한 functional render도 없다.
-function MoviePoster({poster}){
+function MoviePoster({poster, alt}){
     return (
-        <img src={poster} alt="Movie Poster" />
+        <img src={poster} alt={alt} className="Movie__Poster" />
+    )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre}</span>
     )
 }
 
 
-
 //Dumb Component에서 PropTypes를 설정하는 방법
-MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired,
 }
 
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
+}
 
 export default Movie;
